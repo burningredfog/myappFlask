e558a1d66f5c7f3093707d3ab41de97a0ddbf445
burningredfog <burningredfog@gmail.com>
2025-06-15 13:42:59 +0200

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
+import os
 import logging
+
 from flask import Flask, render_template
-from app.extensions import db, login_manager, migrate
 from flask_admin import Admin
-from app.admin import UserAdmin, SecureModelView, CategoryAdmin
-from app.models import User, Post, Category
-from app.utils import markdown_filter
 from flask_mail import Mail
 
-from flask import current_app
+from app.admin import UserAdmin, SecureModelView, CategoryAdmin
+from app.extensions import db, login_manager, migrate
+from app.models import User, Post, Category
+from app.utils import markdown_filter
+
 mail = Mail()
 
-import config  # <-- Dodaj import config jeśli go nie było!
+from app.routes import gallery_bp
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
@@ -21,6 +23,8 @@ login_manager.login_message = "Musisz być zalogowany, aby uzyskać dostęp."
 def create_app(config_class):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    app.register_blueprint(gallery_bp)
 
     app.logger.info(f"App environment: {app.config.get('ENV')}")
     app.logger.info(f"Using database: {app.config.get('SQLALCHEMY_DATABASE_URI')}")
@@ -65,6 +69,10 @@ def create_app(config_class):
     def internal_error(e):
         db.session.rollback()
         return render_template("500.html"), 500
+        
+    @app.errorhandler(413)
+    def request_entity_too_large(error):
+        return render_template('413.html'), 413    
 
     # Automatyczne tworzenie tabel w trybie development
     if app.config.get("ENV") == "development":
@@ -74,6 +82,23 @@ def create_app(config_class):
 
         # Dodaj filtr markdown do Jinja
         app.jinja_env.filters['markdown'] = markdown_filter
+
+    # --- POPRAWIONA FUNKCJA INJECT_SLIDES ---
+    
+    @app.context_processor
+    def inject_slides():
+        slides_dir = os.path.join(app.static_folder, 'slides')
+        # DEBUG:
+        # print("SZUKAM W:", slides_dir)
+        if not os.path.exists(slides_dir):
+            slides = []
+        else:
+            slides = [f'slides/{f}' for f in os.listdir(slides_dir) if
+                      f.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+            # print("ZNALEZIONE:", slides)
+            slides.sort()
+        return dict(slides=slides)
+    # ----------------------------------------
 
     return app
 
